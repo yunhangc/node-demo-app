432656809fbf8fc84ef3d593925f682f451b916d
Edward Viaene <ward.viaene@gmail.com>
2018-03-09 14:23:24 +0100

    v2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('Hello World v2!');
 });
 
 var server = app.listen(3000, function () {
